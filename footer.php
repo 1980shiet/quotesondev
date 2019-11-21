a7522c669b3f9fa18adb934f258e1f57d8ce5762
1980shiet <walsh09chris20@hotmail.com>
2019-11-20 17:09:27 -0800

    Added Red Academy site on footer and changes to the font colors

--- footer.php
+++ footer.php
@@ -14,7 +14,7 @@
 					<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
 				</nav><!-- #site-navigation -->
 				<div class="site-info">
-					<a href="<?php echo esc_url( 'https://wordpress.org/' ); ?>"><?php printf( esc_html( 'Proudly powered by %s' ), 'WordPress' ); ?></a>
+					<p>Brought to you by <a href="https://redacademy.com/programs/?campus=vancouver&ads_cmpid=1478373743&ads_adid=62938959051&ads_matchtype=e&ads_network=g&ads_creative=352325975507&utm_term=red%20academy&ads_targetid=aud-495692225674:kwd-296950416601&utm_campaign=&utm_source=adwords&utm_medium=ppc&ttv=2&gclid=EAIaIQobChMIve7G6Yb65QIVYhh9Ch2fzwO-EAAYASAAEgJ43fD_BwE">Red Academy</a>
 				</div><!-- .site-info -->
 			</footer><!-- #colophon -->
 		</div><!-- #page -->
